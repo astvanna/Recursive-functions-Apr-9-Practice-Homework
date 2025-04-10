e3f7b10e17923ae663edaab8371f883d59d90f23
Anna Astvatsatryan <astvanna@gmail.com>
2025-04-11 01:35:13 +0400

    Added first uppercase recursive function

---
+++ 1st_up_rec.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char find_first_upper(const char *str){
+	if (*str == '\0'){
+		return '\0';
+	}
+	if (*str >= 'A' && *str <= 'Z'){
+		return *str;
+	}
+	return find_first_upper(str + 1);
+}
+
+int main(){
+	char str[] = "lurJes1asUm";
+	printf("First upper is: %c\n", find_first_upper(str));
+	return 0;
+}
